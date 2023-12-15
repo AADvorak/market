2fbd4bbcd57b36ea8774b2d7cce030b1dda1e231
A. Dvorak <dvorak88@mail.ru>
2023-12-15 11:32:38 +0400

    fix: load-user.global.ts navigation

--- frontend/middleware/load-user.global.ts
+++ frontend/middleware/load-user.global.ts
@@ -2,9 +2,7 @@ import {useUser} from "~/stores/user";
 
 export default defineNuxtRouteMiddleware(async (to) => {
   const user= useUser()
-  if (user.user.email) {
-    return
-  } else {
+  if (!user.user.email) {
     await user.load()
   }
   if (!user.user.email) {
